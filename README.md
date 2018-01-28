bfa44abab74b64a88d79308a7a672c9ad297e389
Mihir <gandhi.mi@huksy.neu.edu>
2018-01-28 02:26:02 -0500

    Integrating Spring Boot with MySQL

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cs5200-spring2018-gandhi
+## Integrating Spring Boot with MySQL
+[Insert a static hello message](http://cs5200-spring2018-gandhi.us-east-2.elasticbeanstalk.com/api/hello/insert)
+[Insert a parameterized hello message](http://cs5200-spring2018-gandhi.us-east-2.elasticbeanstalk.com/api/hello/insert/Spring's%20the%20Best)
+[Retrieve all hello messages](http://cs5200-spring2018-gandhi.us-east-2.elasticbeanstalk.com/api/hello/select/all)
+
--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -1,12 +1,59 @@
 package edu.northeastern.cs5200.controllers.hello;
 
 
-
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+
 @RestController
 public class HelloController {
+	
+	@Autowired
+
+	 HelloRepository helloRepository;
+
+	 @RequestMapping("/api/hello/insert")
+
+	 public HelloObject insertHelloObject() {
+
+	  HelloObject obj = new HelloObject("Hello Mihir Gandhi!");
+
+	  helloRepository.save(obj);
+
+	  return obj;
+
+	 }
+	 
+
+		@RequestMapping("/api/hello/select/all")
+		
+		public List<HelloObject> selectAllHelloObjects() {
+		
+		 List<HelloObject> hellos =
+		
+		  (List<HelloObject>)helloRepository.findAll();
+		
+		 return hellos;
+		
+		}
+	
+	 @RequestMapping("/api/hello/insert/{msg}")
+
+	 public HelloObject insertMessage(@PathVariable("msg") String message) {
+
+	  HelloObject obj = new HelloObject(message);
+
+	  helloRepository.save(obj);
+
+	  return obj;
+
+	 }
+	 
+	 
+	 
 	@RequestMapping("/api/hello/string")
 	public String sayHello() {
 		return "Hello Mihir Gandhi!";
